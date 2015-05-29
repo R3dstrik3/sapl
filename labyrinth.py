1f04266c32e8cb1c3f6c74fe939a2aa9ad7d6f49
DominikRidder <d.ridder@fz-juelich.de>
2015-05-29 11:54:52 +0200

    update

--- labyrinth.py
+++ labyrinth.py
@@ -13,11 +13,29 @@ class Point:
 position = Point(0,0)
 
 def getsteps():
-      return (1, 2, 3)
+      return (Point(1,0), Point(0,1), Point(-1,0), Point(0,-1))
 
-def rekursion(last_step):
+def rekursion(last_step, position):
+      print position
+      for line in Matrix:
+            print(line)
       for step in getsteps():
-            print(step)
+            if last_step.x != step.x | last_step.y != step.y:
+                  try:
+                        if Matrix[position.x+step.x][position.y+step.y] == ' ':
+                              Matrix[position.x+step.x][position.y+step.y] = 'E'
+                              Matrix[position.x][position.y] = ' '
+                              position = Point(position.x+step.x, position.y+step.y)
+                              if rekursion(Point(-step.x,-step.y), position) == 1:
+                                    return 1
+                              Matrix[position.x][position.y] = ' '
+                              position = Point(position.x-step.x, position.y-step.y)
+                              Matrix[position.x][position.y] = 'E'
+                        elif Matrix[position.x+step.x][position.y+step.y] == 'A':
+                              return 1
+                  except IndexError:
+                        print()
+      return 0
             
 if len(sys.argv) < 2:
       print("Not enought arguments.")
@@ -31,7 +49,7 @@ height = 0
 with open (sys.argv[1]) as f:
       for line in f:
             try:
-                  position = Point(line.rindex("E"), height+1)
+                  position = Point(height, line.rindex("E"))
             except ValueError:
                   print("nope not in this line")
             height = height + 1
@@ -45,9 +63,17 @@ width = width - 1
 print("width =",width)
 print("height =",height)
 print(position.x,position.y)
-rekursion(0)
 
-Matrix = [[0 for x in range(width)] for x in range(height)]
+Matrix = [[0 for x in range(width+1)] for x in range(height)]
+x = -1
+y = 0
 with open (sys.argv[1]) as f:
       for line in f:
-            
+            x = x + 1
+            y = 0
+            for char in line:
+                  Matrix[x][y] = char
+                  y = y + 1
+
+
+print rekursion(Point(0,0), position)
